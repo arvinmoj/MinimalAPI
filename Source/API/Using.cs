d767412c31dc12af426ffd497205b03aa47202c2
arvinmoj <arvinmojaverian@hotmail.com>
2024-08-08 15:24:28 +0330

    add: todo api handler

---
+++ Source/API/Using.cs
@@ -0,0 +1,21 @@
+global using Data;
+global using API.Utils;
+global using API.Handlers;
+global using Models.Items;
+global using ViewModels.Users;
+global using ViewModels.Items;
+global using ViewModels.Results;
+global using ViewModels.Validators;
+
+global using Asp.Versioning;
+global using System.Diagnostics;
+global using System.Net.Http.Headers;
+global using System.Threading.RateLimiting;
+
+global using System.Security.Claims;
+global using System.IdentityModel.Tokens.Jwt;
+
+global using FluentValidation;
+global using Microsoft.OpenApi.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
